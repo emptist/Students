36685ae78aeeed1b3aeba341c6aa92e4e340e379
emptist <jigme1968@gmail.com>
2020-12-07 05:48:09 +0800

    add a label

--- Shared/View/StudentDetail.swift
+++ Shared/View/StudentDetail.swift
@@ -9,15 +9,37 @@ import SwiftUI
 
 struct StudentDetail: View {
     var student: Student
+    @State private var zoomed = false
     
     var body: some View {
         VStack {
+            Spacer()
             Image(student.imageName)
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .navigationTitle("Student")
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation {
+                        zoomed.toggle()
+                    }
+                }
+            Spacer()
+            
+            if student.gender == "男" {
+                HStack {
+                    Spacer()
+                    Label("Male", systemImage:"flame.fill")
+                    Spacer()
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .font(Font/*@START_MENU_TOKEN@*/.headline/*@END_MENU_TOKEN@*/.smallCaps())
+                .background(Color.red)
+                .foregroundColor(.yellow)
+            } else {
+                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+            }
         }
+        .navigationTitle("Student")
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
